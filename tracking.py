3615e78a70cbdef1a5250bff4c055f71f9f4c375
Avdhoot <avdhootu.27@gmail.com>
2021-12-24 12:21:32 +0530

    Object selection

--- tracking.py
+++ tracking.py
@@ -1,2 +1,16 @@
 import cv2
 
+cap = cv2.VideoCapture(0)
+
+tracker = cv2.legacy.TrackerMOSSE_create()
+success, img = cap.read()
+
+bbox = cv2.selectROI("Tracking", img, False)
+tracker.init(img, bbox)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Tracking", img)
+
+    if cv2.waitKey(1) & 0xff==ord('q'):
+        break
